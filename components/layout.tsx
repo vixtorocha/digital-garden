997d97b7d0e8510383d6074d7f36a4df7547c4eb
João Victor Rocha <joaobrvictor@gmail.com>
2021-02-23 17:35:13 -0300

    fix layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Layout = ({ children }) => {
-  return <div className="p-5 md:p-10 lg:max-w-4xl mx-auto">{children}</div>;
+  return <div className="px-5 md:px-10 lg:max-w-4xl mx-auto">{children}</div>;
 };
 export default Layout;
