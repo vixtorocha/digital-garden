ba8b603fee9d000db9a3d87434ad9b1d7f1dbe6b
João Victor Rocha <joaobrvictor@gmail.com>
2021-02-28 10:49:26 -0300

    Update index text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "components/Link";
 
 export default function Home() {
   return (
@@ -9,9 +10,18 @@ export default function Home() {
       </Head>
 
       <main className="">
+        <p>Olá!!!</p>
         <p>
-          Hi! I'm João Victor Rocha, I'm a Software Engineer currently studying
-          at the Federal University of Goiás (UFG) - Goiânia, Brazil
+          Meu nome é João Victor Rocha e eu sou estudante de Engenharia de
+          Software na{" "}
+          <Link href="https://www.ufg.br/">Universidade Federal de Goiás</Link>.
+        </p>
+        <p>
+          Você pode me encontrar no{" "}
+          <Link href="https://twitter.com/vixtorocha">Twitter</Link>, no{" "}
+          <Link href="https://www.linkedin.com/in/vixtorocha/">LinkedIn</Link> e
+          ver meus projetos no{" "}
+          <Link href="https://github.com/vixtorocha">Github</Link>.
         </p>
       </main>
     </div>
