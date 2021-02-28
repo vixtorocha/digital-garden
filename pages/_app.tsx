06f58b656ee55f2c0e323cd8e0fdff2d53aeabe1
João Victor Rocha <joaobrvictor@gmail.com>
2021-02-28 00:08:20 -0300

    Adds poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,27 @@
+import React from "react";
+import Head from "next/head";
 import "../styles/globals.css";
 import Layout from "../components/layout";
 import Header from "../components/header";
+// import "styles/fonts/stylesheet.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Header />
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <link
+          href="/fonts/stylesheet.css"
+          rel="stylesheet preload"
+          as="style"
+        />
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+      </Head>
+      <Layout>
+        <Header />
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 
