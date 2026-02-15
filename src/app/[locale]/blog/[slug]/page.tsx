570d9ccd88564c24f4baeebcea1546be45646bf2
João Victor Rocha <joaobrvictor@gmail.com>
2026-02-14 21:22:04 -0300

    feat: Add blog post on PostgreSQL features and update blog metadata handling

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -1,30 +1,46 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getLocale } from "next-intl/server";
+import { Metadata } from "next";
 
 type Props = {
   readonly params: { readonly slug: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const locale = await getLocale();
+  const { slug } = await params;
+
+  const markdownWithMeta = fs.readFileSync(
+    path.join("src/posts", `${locale}/${slug}.mdx`),
+    "utf-8",
+  );
+
+  const { data } = matter(markdownWithMeta);
+
+  return {
+    title: data.title,
+    description: data.description ?? undefined, // optional
+  };
+}
 
 export default async function BlogPostPage({ params }: Props) {
   const locale = await getLocale();
   const { slug } = await params;
   const markdownWithMeta = fs.readFileSync(
     path.join("src/posts", `${locale}/${slug}.mdx`),
-    "utf-8"
+    "utf-8",
   );
   const { data, content } = matter(markdownWithMeta);
-  const mdxSource = await serialize(content);
 
   return (
     <div>
       <h1 className="text-4xl font-bold mb-4">{data.title}</h1>
       <p className="text-gray-600 mb-8">{data.date}</p>
       <article className="mdx prose lg:prose-xl dark:prose-invert">
-        <MDXRemote {...mdxSource} source={content} />
+        <MDXRemote source={content} />
       </article>
     </div>
   );
