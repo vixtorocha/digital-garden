66a845dfc30457d78bf3597ef836e9ba6bbc5697
João Victor Rocha <joaobrvictor@gmail.com>
2026-03-15 16:15:34 -0300

    feat: adds description to posts

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,12 @@
 import fs from 'fs'; // File system module to read files
 import path from 'path'; // Path module to handle file paths
 import matter from 'gray-matter';
-import { getLocale } from 'next-intl/server';
+import { getLocale, getTranslations } from 'next-intl/server';
 
 type FrontMatter = {
   title: string;
   date: string;
+  description: string;
 };
 
 type Post = {
@@ -32,17 +33,21 @@ async function getPosts(locale: string): Promise<Post[]> {
 export default async function Home() {
   const locale = await getLocale();
   const posts = await getPosts(locale);
+  const t = await getTranslations('home');
 
   return (
     <div>
-      <h1 className='text-2xl font-bold mb-8'>Blog</h1>
+      <h1 className='text-xl font-medium mb-8 mt-8 text-rose-500'>BLOG</h1>
       <div>
         {posts.map((post: Post) => (
-          <div key={post.slug} className='mb-4'>
-            <a href={`/blog/${post.slug}`} className='hover:underline mb-0 block decoration-yellow-500 decoration-2'>
+          <div key={post.slug} className='mb-6'>
+            <a href={`/blog/${post.slug}`} className='hover:underline mb-2 block decoration-yellow-500 decoration-2'>
               <h2 className='text-xl mb-0 mt-0 font-semibold'>{post.data.title}</h2>
             </a>
-            <p className='text-gray-600'>{post.data.date}</p>
+            <p className='text-gray-900 mb-2 text-base'>{post.data.description}</p>
+            <a href={`/blog/${post.slug}`} className='font-medium hover:underline decoration-yellow-500 decoration-2'>
+              {t('readMore')} -&gt;
+            </a>
           </div>
         ))}
       </div>
